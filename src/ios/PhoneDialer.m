b7cd8671c9f9e119b7ece7683dfc2b6e726bd2cc
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2023-04-19 10:18:28 +0100

    Update PhoneDialer.m

--- src/ios/PhoneDialer.m
+++ src/ios/PhoneDialer.m
@@ -94,71 +94,30 @@ BOOL monitorAudioRouteChange = NO;
 - (void)call:(CDVInvokedUrlCommand*)command
 {
     [self.commandDelegate runInBackground:^{
-
-        CDVPluginResult* pluginResult = nil;
-
-        if (![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel://"]]) {            
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"feature"];
-        } else {
-            
-            NSLog(@"Batatas 1");
-            NSString* url;
-            NSString* number = [command.arguments objectAtIndex:0];
-            NSString* appChooser = [command.arguments objectAtIndex:1];
-            NSString* IsSpeakerOn = [command.arguments objectAtIndex:2];
-
-            if (number != nil && [number length] > 0) {
-                if ([number hasPrefix:@"tel:"] || [number hasPrefix:@"telprompt://"]) {
-                    url = number;
-                } else {
-                    // escape characters such as spaces that may not be accepted by openURL
-                    url = [NSString stringWithFormat:@"tel:%@",                    
-                    [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-                }                                       
-
-                //detect Audio Route Changes to make speakerOn and speakerOff event handlers
-                // [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleAudioRouteChange:) name:AVAudioSessionRouteChangeNotification object:nil];                            
         
-                // openURL is expected to fail on devices that do not have the Phone app, such as simulators, iPad, iPod touch
-                NSLog(@"Batatas 2");
-                if(![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:url]]) {
-                    NSLog(@"Batatas 3");
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"feature"];
-                }
-                else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]]) {
-                    NSLog(@"Batatas 4");
-                    // missing phone number
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"notcall"];
-                } else {
-                    NSLog(@"Batatas 5");
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-                }
-
-                if([[IsSpeakerOn lowercaseString] isEqualToString: @"true"]){
-                    AVAudioSession *sessionInstance = [AVAudioSession sharedInstance];
-                    BOOL success = [sessionInstance overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:nil];
-                    NSLog(@"Batatas 6");
-                    if (success) {
-                        NSLog(@"Configuring Speaker On");      
-                    }               
-                } else {
-                    NSLog(@"Batatas 7");
-                    AVAudioSession *sessionInstance = [AVAudioSession sharedInstance];
-                    BOOL success = [sessionInstance overrideOutputAudioPort:AVAudioSessionPortOverrideNone error:nil];                                        
-                    if (success) {
-                        NSLog(@"Configuring Speaker Off");      
-                    }
-                }    
-
-            } else {
-                NSLog(@"Batatas 8");
-                // missing phone number
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"empty"];
-            }
+        __block CDVPluginResult* pluginResult = nil;
+        NSString* number = [command.arguments objectAtIndex:0];
+        number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+        if(![number hasPrefix:@"tel:"]){
+            number =  [NSString stringWithFormat:@"tel:%@", number];
         }
-        NSLog(@"Batatas 9");
+
+        // run in mainthread as below 
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if(![CFCallNumber available]) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+            }
+            else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
+                // missing phone number
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
+            } else {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            }
+        });
         // return result
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        
     }];
 }
 
